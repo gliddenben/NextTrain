d6f906df60b89be150db37642b94fe61875b8d27
Ben Glidden <gliddenben@gmail.com>
2024-03-02 15:23:14 -0500

    Initial Commit

---
+++ NextTrain/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NextTrain
+//
+//  Created by Ben Glidden on 3/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NextTrain/NextTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextTrainApp.swift
+//  NextTrain
+//
+//  Created by Ben Glidden on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct NextTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
