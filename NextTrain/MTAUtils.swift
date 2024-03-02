43931acd27fba6798acfbccf91a1010f9e92f328
Ben Glidden <gliddenben@gmail.com>
2024-03-02 16:46:16 -0500

    implemented getColorForTrain

--- NextTrain/MTAUtils.swift
+++ NextTrain/MTAUtils.swift
@@ -38,3 +38,54 @@ func getColorForLine(line: MTALine) -> Color {
     return Color(UIColor(named: "MTAColors/S")!)
   }
 }
+
+enum MTATrain {
+    case one
+    case two
+    case three
+    case four
+    case five
+    case six
+    case seven
+    case a
+    case c
+    case e
+    case b
+    case d
+    case f
+    case m
+    case g
+    case j
+    case z
+    case l
+    case n
+    case q
+    case r
+    case w
+    case s
+}
+
+func getLineForTrain(train: MTATrain) -> MTALine {
+    switch train {
+    case .a, .c, .e:
+        return .ace
+    case .one, .two, .three:
+        return .oneTwoThree
+    case .four, .five, .six:
+        return .fourFiveSix
+    case .seven:
+        return MTALine.seven
+    case .b, .d, .f, .m:
+        return .bdfm
+    case .g:
+        return MTALine.g
+    case .j, .z:
+        return .jz
+    case .l:
+        return MTALine.l
+    case .n, .q, .r, .w:
+        return .nqrw
+    case .s:
+        return MTALine.s
+    }
+}
