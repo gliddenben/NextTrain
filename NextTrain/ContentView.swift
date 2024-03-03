cf31e05225906a3922b462cdf05b2b53ba71557b
Ben Glidden <gliddenben@gmail.com>
2024-03-03 12:34:10 -0500

    changed the train badges to horizontal scroll and added the Station Name icon with hamburger menu

--- NextTrain/ContentView.swift
+++ NextTrain/ContentView.swift
@@ -10,8 +10,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            ScrollView {
-                VStack {
+            HStack {
+                Image(systemName: "line.horizontal.3")
+                    .imageScale(.large)
+                    .padding(.leading, 10)
+                Text("Station Name")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(15)
+            }.overlay(
+                RoundedRectangle(cornerRadius: 10.0)
+                    .stroke(Color.black, lineWidth: 2.0))
+            .padding(5)
+            ScrollView(.horizontal) {
+                HStack {
                     ForEach(MTATrain.allCases, id: \.self) {train in
                         TrainBadge(train: train)}
                 }
